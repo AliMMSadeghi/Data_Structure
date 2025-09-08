9ec5356e7e7293098f2f1320fcbad0bd45a81860
AliMMSadeghi <alimmsadeghi@vt.edu>
2025-09-08 08:55:16 -0400

    Add linear search

---
+++ Binary search/LinearSearch.cpp
@@ -0,0 +1,38 @@
+// Linear Search
+
+#include<iostream>
+using namespace std;
+///////////////////////////////
+int find(int a[], int n, int key)
+{
+
+    for(int i=0; i<=n-1; i++)
+        if (key ==a[i])
+           return i;
+          
+    return -1;
+
+}
+///////////////////////////////
+int main()
+{
+    int a[] = {19, 20, 13, 20, 6, 14}; // Each integer allocates 4 bite of memory
+
+    int n;
+    n=sizeof(a)/sizeof(a[0]);
+    // cout << n << endl;
+
+    int key = 20;
+    int p;
+    
+    p = find(a, n , key);
+
+
+    if(p==-1)
+        cout << "not found" << endl;
+    else
+        cout << p << endl;
+
+     return 0;
+
+}
